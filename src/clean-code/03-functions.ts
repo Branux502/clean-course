1cca1b2c04c6e2974a2ba547bcb1acf11db3c4f9
root <root@bmiranda>
2024-12-11 21:27:50 -0600

    Clase 17, ejercicio de funciones

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,41 @@
+(() => {
+
+    function getMovieById( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    function getActorBioById( ActorId: string ) {
+        console.log({ ActorId });
+    }
+    
+    interface Movie{
+        cast: string[];
+        description:string;
+        rating:number;
+        title: string;
+    }
+
+    // Crear una película
+    function createMovie({title, description, rating, cast}:Movie) {
+        console.log({ title, description, rating, cast });
+    }
+
+    function  checkFullName(fullName:string){
+        console.log({fullName});
+        return true;
+    }
+
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+    
+        if ( checkFullName(fullName) ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+})();
